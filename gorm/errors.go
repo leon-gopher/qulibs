6679175e6784824a18530fddf17af1f1a634ea3a
leon-yc <leon302816@gmail.com>
2022-08-15 23:31:54 +0800

    gorm v1 delete

--- gorm/errors.go
+++
@@ -1,9 +0,0 @@
-package gorm
-
-import "errors"
-
-// errors
-var (
-	ErrNotFoundConfig = errors.New("no config found")
-	ErrInvalidConfig  = errors.New("named config is not a valid *Config type")
-)
